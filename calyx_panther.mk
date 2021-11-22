ca0f3676a3dd0b5ee96393470fd88e049c14ec6e
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit bc88fcd262e70cac89223b460611b2fc444d4b13
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:25:25 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: If3f761b8178b1663b73eb705d77105bd30ec15a9
        (cherry picked from commit 8d48d9ea51645e468a015465b919c365ecb7da90)
    
    commit a3196e48fe1795cc6acbe37ff847eec6ebc13a9c
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Dec 3 03:43:51 2020 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: I3221d4d5e29f5beb6554d0c280dec546d29f2c08
        (cherry picked from commit 7266f839ce08df3aa61cc80f28afff3c0d3f77ac)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ calyx_panther.mk
@@ -0,0 +1,20 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/gs201/calyx_common.mk)
+$(call inherit-product, device/google/pantah/device-calyx.mk)
+$(call inherit-product, device/google/pantah/aosp_panther.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_panther
+PRODUCT_MODEL := Pixel 7
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=panther \
+    PRIVATE_BUILD_DESC="panther-user 14 UP1A.231005.007 10754064 release-keys"
+
+BUILD_FINGERPRINT := google/panther/panther:14/UP1A.231005.007/10754064:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
