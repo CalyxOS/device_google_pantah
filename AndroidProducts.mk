CalyxOS/device_google_raviole

Add initial Calyx product makefiles
Also includes:
commit bc88fcd262e70cac89223b460611b2fc444d4b13
Author: Chirayu Desai <chirayudesai1@gmail.com>
Date:   Thu Nov 12 03:25:25 2020 +0530

    calyx: Inherit device-calyx.mk

    Change-Id: If3f761b8178b1663b73eb705d77105bd30ec15a9
    (cherry picked from commit 8d48d9ea51645e468a015465b919c365ecb7da90)

commit a3196e48fe1795cc6acbe37ff847eec6ebc13a9c
Author: Chirayu Desai <chirayudesai1@gmail.com>
Date:   Thu Dec 3 03:43:51 2020 +0530

    Add overlay-calyx to the list of overlays

    Change-Id: I3221d4d5e29f5beb6554d0c280dec546d29f2c08
    (cherry picked from commit 7266f839ce08df3aa61cc80f28afff3c0d3f77ac)

Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

AndroidProducts.mk
@@ -31,7 +31,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_whitefin_pkvm.mk \
     $(LOCAL_DIR)/aosp_whitefin_hwasan.mk \
     $(LOCAL_DIR)/aosp_whitefin_64.mk \
-    $(LOCAL_DIR)/factory_whitefin.mk
+    $(LOCAL_DIR)/factory_whitefin.mk \
+    $(LOCAL_DIR)/calyx_oriole.mk \
+    $(LOCAL_DIR)/calyx_raven.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-userdebug \

device-calyx.mk
@@ -1,0 +1,1 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/raviole/overlay-calyx

CalyxOS/device_google_pantah

AndroidProducts.mk
@@ -22,7 +22,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_cheetah.mk \
     $(LOCAL_DIR)/factory_cheetah.mk \
     $(LOCAL_DIR)/aosp_panther.mk \
-    $(LOCAL_DIR)/factory_panther.mk
+    $(LOCAL_DIR)/factory_panther.mk \
+    $(LOCAL_DIR)/calyx_cheetah.mk \
+    $(LOCAL_DIR)/calyx_panther.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_cloudripper-userdebug \

device-calyx.mk
@@ -1,0 +1,1 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-calyx