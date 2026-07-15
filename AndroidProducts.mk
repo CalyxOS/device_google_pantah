CalyxOS/device_google_raviole

Initial CalyxOS makefiles
Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

AndroidProducts.mk
@@ -6,5 +6,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_oriole.mk \
     $(LOCAL_DIR)/aosp_raven.mk \
+    $(LOCAL_DIR)/calyx_oriole.mk \
+    $(LOCAL_DIR)/calyx_raven.mk \
     $(LOCAL_DIR)/lineage_oriole.mk \
     $(LOCAL_DIR)/lineage_raven.mk

CalyxOS/device_google_pantah

AndroidProducts.mk
@@ -6,5 +6,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_cheetah.mk \
     $(LOCAL_DIR)/aosp_panther.mk \
+    $(LOCAL_DIR)/calyx_cheetah.mk \
+    $(LOCAL_DIR)/calyx_panther.mk \
     $(LOCAL_DIR)/lineage_cheetah.mk \
     $(LOCAL_DIR)/lineage_panther.mk